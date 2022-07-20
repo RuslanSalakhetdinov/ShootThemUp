02936ea84a920208b39ded2f25823fccd197bb38
Ruslan Salakhetdinov <79852825377@yandex.ru>
2022-07-20 17:34:21 +0300

    Delete .gitignore files

--- Intermediate/Build/Win64/ShootThemUpEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.32.31332
-#include "C:/Users/allesklar09/Unreal Projects/MyTestProjects/ShootThemUp/Intermediate/Build/Win64/ShootThemUpEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
