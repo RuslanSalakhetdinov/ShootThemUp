34da728e540faaad200808e9f14a94a841d1cf3c
Ruslan Salakhetdinov <79852825377@yandex.ru>
2022-07-21 17:58:48 +0300

    Lesson 33. UHeathComponent

--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -8,6 +8,8 @@
 
 class UCameraComponent;
 class USpringArmComponent;
+class USTUHealthComponent;
+class UTextRenderComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
@@ -25,8 +27,15 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	UCameraComponent* CameraComponent;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USTUHealthComponent* HealthComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UTextRenderComponent* HealthTextComponent;
+
 	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	virtual void
+	BeginPlay() override;
 
 public:
 	// Called every frame
