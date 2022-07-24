719e1609a217b5913109ebb57ca4c4ce07d3fb8b
Ruslan Salakhetdinov <79852825377@yandex.ru>
2022-07-24 21:45:13 +0300

    Lesson 38. Add Spectator mode. (ASpectatorPawn)

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -7,6 +7,7 @@
 #include <Components/STUCharacterMovementComponent.h>
 #include <Components/STUHealthComponent.h>
 #include <Components/TextRenderComponent.h>
+#include <GameFramework/Controller.h>
 
 DEFINE_LOG_CATEGORY_STATIC(BaseCharacterLog, All, All);
 
@@ -111,10 +112,12 @@ void ASTUBaseCharacter::OnDeath()
 	UE_LOG(BaseCharacterLog, Display, TEXT("Player %s is dead =("), *GetName());
 
 	PlayAnimMontage(DeathAnimMontage);
-
 	GetCharacterMovement()->DisableMovement();
-
 	SetLifeSpan(5.0f);
+	if (Controller)
+	{
+		Controller->ChangeState(NAME_Spectating);
+	}
 }
 
 void ASTUBaseCharacter::OnHealthChanged(float Health)
