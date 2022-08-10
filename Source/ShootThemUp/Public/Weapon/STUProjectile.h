b7af5ba8227f30673b757c14e8ff482a8d3c176c
Ruslan Salakhetdinov <79852825377@yandex.ru>
2022-08-10 22:42:52 +0300

    Lessons 54-55. Modify LauncherWeapon type, spawn Projectile, add ProjectileMovementComponent. Set params.

---
+++ Source/ShootThemUp/Public/Weapon/STUProjectile.h
@@ -0,0 +1,33 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASTUProjectile();
+
+	void SetShotDirection(const FVector& Direction);
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	USphereComponent* CollisionComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	UProjectileMovementComponent* MovementComponent;
+
+	virtual void BeginPlay() override;
+
+private:
+	FVector ShotDirection;
+};
